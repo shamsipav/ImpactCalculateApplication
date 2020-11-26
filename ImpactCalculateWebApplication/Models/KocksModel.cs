1bcc58d1f6c9c105f8fa7db97bdafad8c9f3d531
Егор <egor.demyanenko2000@mail.ru>
2020-11-26 23:05:12 +0300

    KocksModel

---
+++ ImpactCalculateWebApplication/Models/KocksModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImpactCalculateWebApplication.Models
+{
+    public class KocksModel
+    {
+        public Kocks Kemerovo_3_4 { get; set; }
+        public Kocks Kemerovo_60_1_2 { get; set; }
+
+        public Kocks Gubaha_3_4 { get; set; }
+        public Kocks Gubaha_60_1_2 { get; set; }
+
+        public Kocks Chehiya_3_4 { get; set; }
+        public Kocks Chehiya_60_90_1_2 { get; set; }
+
+        public Kocks Harkov_3_4 { get; set; }
+        public Kocks Harkov_60_1_2 { get; set; }
+
+        public Kocks Alza_Invest_1_2 { get; set; }
+
+    }
+
+
+    public class Kocks
+    {
+
+    }
+}
