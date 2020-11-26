9942e937d6303d5b9be623f3460387d59c4a62e0
Егор <egor.demyanenko2000@mail.ru>
2020-11-26 23:08:17 +0300

    CocksModel :c

--- ImpactCalculateWebApplication/Models/KocksModel.cs
+++ ImpactCalculateWebApplication/Models/KocksModel.cs
@@ -4,26 +4,26 @@ using System.Text;
 
 namespace ImpactCalculateWebApplication.Models
 {
-    public class KocksModel
+    public class CocksModel
     {
-        public Kocks Kemerovo_3_4 { get; set; }
-        public Kocks Kemerovo_60_1_2 { get; set; }
+        public Cocks Kemerovo_3_4 { get; set; }
+        public Cocks Kemerovo_60_1_2 { get; set; }
 
-        public Kocks Gubaha_3_4 { get; set; }
-        public Kocks Gubaha_60_1_2 { get; set; }
+        public Cocks Gubaha_3_4 { get; set; }
+        public Cocks Gubaha_60_1_2 { get; set; }
 
-        public Kocks Chehiya_3_4 { get; set; }
-        public Kocks Chehiya_60_90_1_2 { get; set; }
+        public Cocks Chehiya_3_4 { get; set; }
+        public Cocks Chehiya_60_90_1_2 { get; set; }
 
-        public Kocks Harkov_3_4 { get; set; }
-        public Kocks Harkov_60_1_2 { get; set; }
+        public Cocks Harkov_3_4 { get; set; }
+        public Cocks Harkov_60_1_2 { get; set; }
 
-        public Kocks Alza_Invest_1_2 { get; set; }
+        public Cocks Alza_Invest_1_2 { get; set; }
 
     }
 
 
-    public class Kocks
+    public class Cocks
     {
 
     }
